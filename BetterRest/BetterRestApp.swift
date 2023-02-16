94a747ce704e44ea18e98f1ba53b586c6e68caf6
Tiago Baptista Noronha <tiago.bn@gmail.com>
2023-02-16 10:31:52 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Tiago Baptista Noronha on 16/02/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
